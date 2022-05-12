4056166614e331844f5b8328f734e36f0c446ddf
Daniela Farias <farias.danicris@gmail.com>
2022-05-12 16:12:14 -0300

    Subscribe button

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+
+
+export function SubscribeButton() {
+    return (
+        <button className={styles.subscribeButton} type="button">
+            Subscribe now
+        </button>
+    );
+}
