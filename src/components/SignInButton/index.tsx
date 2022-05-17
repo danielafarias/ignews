349ee8721880717efadcaf7de80a80a0a7c550ad
Daniela Farias <farias.danicris@gmail.com>
2022-05-17 11:52:23 -0300

    SignIn, SignOut, Session Provider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,19 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
-        <button className={styles.signInButton} type="button">
+    const session  = useSession();
+    console.log(session)
+    return session.data ? (
+        <button className={styles.signInButton} type="button" onClick={() => signOut()}>
             <FaGithub color="#04d361"/>
-            Daniela Farias
+            {session.data.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button className={styles.signInButton} type="button">
+        <button className={styles.signInButton} type="button" onClick={() => signIn('github')}>
             <FaGithub color="#eba417"/>
             Sign in with Github
         </button>
