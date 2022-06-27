76f7198b7fd02ebdfdc348b000436fdeb5c87ac8
Daniela Farias <farias.danicris@gmail.com>
2022-06-27 12:37:51 -0300

    Home page test

---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../../pages";
+
+jest.mock("next/router");
+
+jest.mock("next-auth/react", () => {
+    return {
+      useSession() {
+        return [null, false]
+      },
+    };
+  });
+
+describe('Home page', () => {
+    it("renders correctly", () => {
+       render(
+        <Home product={{priceId: "fake", amount: "R$10,00"}} />
+      );
+    
+      expect(screen.getByText('for R$10,00 month')).toBeInTheDocument()
+    });
+})
+
