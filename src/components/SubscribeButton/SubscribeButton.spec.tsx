ed9aacfb66693c25eaba21158468415454451ada
Daniela Farias <farias.danicris@gmail.com>
2022-06-27 12:15:01 -0300

    SubscribeButton Test

---
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -0,0 +1,79 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { signIn, useSession } from "next-auth/react";
+import { SubscribeButton } from ".";
+import { useRouter } from "next/router";
+
+jest.mock("next-auth/react");
+jest.mock("next/router");
+
+describe("SubscribeButton Component", () => {
+  it("renders correctly", () => {
+    const useSessionMocked = jest.mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({
+        data: null,
+        status: "unauthenticated",
+    });
+
+    render(<SubscribeButton />);
+
+    expect(
+      screen.getByRole("button", { name: "Subscribe now" })
+    ).toBeInTheDocument();
+  });
+
+  it("redirects user to sign in when not authenticated", () => {
+    const useSessionMocked = jest.mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({
+        data: null,
+        status: "unauthenticated",
+    });
+    
+    const signInMocked = jest.mocked(signIn);
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByRole("button", {
+      name: "Subscribe now",
+    });
+
+    fireEvent.click(subscribeButton);
+
+    expect(signInMocked).toHaveBeenCalled();
+  });
+
+  it("redirects to posts when user already has subscription", () => {
+    const useRouterMocked = jest.mocked(useRouter);
+
+    const pushMocked = jest.fn();
+
+    const useSessionMocked = jest.mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({
+      data: {
+        user: {
+          name: "John Doe",
+          email: "john.doe@example.com",
+        },
+        expires: "fake-expires",
+        activeSubscription: "fake-act",
+      },
+      status: "authenticated",
+    });
+
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMocked,
+    } as any);
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByRole("button", {
+      name: "Subscribe now",
+    });
+
+    fireEvent.click(subscribeButton);
+
+    expect(pushMocked).toHaveBeenCalled();
+  });
+});
